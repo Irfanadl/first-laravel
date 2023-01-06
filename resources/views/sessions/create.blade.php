a2128d0bd1ba7ac8b18fb36d1f7f20db5c70568a
Irfanadl <Irfan.adl410622@gmail.com>
2023-01-06 15:31:26 +0800

    Ep50

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,47 +1,9 @@
 <x-layout>
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 p-6 rounded-xl border border-gray-200">
-            <h1 class="text-center font-bold text-sl">Register</h1>
-            <form action="/register" method="post" class="mt-10">
+            <h1 class="text-center font-bold text-sl">Log In</h1>
+            <form action="/login" method="post" class="mt-10">
                 @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 text-xs font-bold text-gray-700 uppercase"
-                           for="name">
-                        {{ __('name') }}
-                    </label>
-                
-                    <input class="w-full p-2 border border-gray-400 rounded"
-                           type="text"
-                           id="name"
-                           name="name"
-                           value="{{ old('name') }}"
-                           required
-                    >
-
-                    @error("name")
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-
-                <div class="mb-6">
-                    <label class="block mb-2 text-xs font-bold text-gray-700 uppercase"
-                           for="username">
-                        {{ __('username') }}
-                    </label>
-                
-                    <input class="w-full p-2 border border-gray-400 rounded"
-                           type="text"
-                           id="username"
-                           name="username"
-                           value="{{ old('username') }}"
-                           required
-                    >
-
-                    @error("username")
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
 
 
                 <div class="mb-6">
@@ -87,7 +49,7 @@
                 <div class="mb-6">
                     <button type="submit"
                            class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
+                        Log In
                     </button>
                 </div>
                 
