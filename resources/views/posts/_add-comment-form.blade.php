099ccfaa189abe20bdc6e88cb4c01a6f905fb857
Irfanadl <Irfan.adl410622@gmail.com>
2023-03-09 11:43:18 +0800

    Ep57

---
+++ resources/views/posts/_add-comment-form.blade.php
@@ -0,0 +1,27 @@
+@auth
+    <x-panel>
+        <form method="POST" action="/posts/{{ $post->slug }}/comments">
+            @csrf
+
+            <header class="flex items-center">
+                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+                <h2 class="ml-3">Want to participate?</h2>
+            </header>
+
+            <div class="mt-6">
+                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="10" placeholder="Quick, think of something to say!" required></textarea>
+                @error('body')
+                    <span class="text-xs text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="flex justify-end">
+                <x-submit-button>Post</x-submit-button>
+            </div>
+        </form>
+    </x-panel>
+@else
+    <p class="font-bold">
+        <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Log in</a>  to leave a comment
+    </p>
+@endauth
