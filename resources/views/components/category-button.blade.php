636cf7eb95e267e568b7866994c0fcf31ec13986
Irfanadl <Irfan.adl410622@gmail.com>
2023-03-21 12:00:06 +0800

    test

--- resources/views/components/category-button.blade.php
+++ resources/views/components/category-button.blade.php
@@ -2,5 +2,5 @@
 
 <a href="/categories/{{ $category->slug }}"
    class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-   style="font-size: 30px"
+   style="font-size: 10px"
 >{{ $category->name }}</a>
